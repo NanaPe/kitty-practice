d8eb49b9088287eb5e6132a3876a14f75d010e3f
NanaPe <plutomario@proton.me>
2023-07-13 23:53:20 +0700

    3rd

---
+++ routes/catRoutes.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const catController = require('../controllers/catController')
+
+const router = express.Router()
+
+router
+ .route('/')
+ .get(catController.getAllCats)
+
+ router
+  .route('/upload')
+  .get(catController.uploadPage)
+  .post(catController.upload.single('image'),catController.createCat)
+
+
+
+  router
+  .route('/edit/:id')
+  .post(catController.editPage)
+  .post(catController.updateCat)
+
+  router
+  .route('/delete/:id')
+  .post(catController.deleteCat)
+
+  module.exports = router
